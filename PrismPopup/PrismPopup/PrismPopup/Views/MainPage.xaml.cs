0142d60546c736376e50aed24f6fb55ec5ca159d
IoTFier <Nilesh@IoTFier.com>
2020-07-06 19:36:21 -0500

    Code clean up.

--- PrismPopup/PrismPopup/PrismPopup/Views/MainPage.xaml.cs
+++ PrismPopup/PrismPopup/PrismPopup/Views/MainPage.xaml.cs
@@ -17,7 +17,6 @@ namespace PrismPopup.Views
     {
         private const string  RTLLanguage = "ar";
 
-
         public MainPage()
         {
             InitializeComponent();
@@ -25,41 +24,13 @@ namespace PrismPopup.Views
         }
         public void SetRTL(string lang)
         {
-            if (CultureService.GetCultureForLanguage().Equals(RTLLanguage))
-            //if (lang == "ar")
-            {
-                Device.BeginInvokeOnMainThread(() =>
-                {
-                    //this.FlowDirection = FlowDirection.RightToLeft;
-                    popup.FlowDirection = FlowDirection.RightToLeft;
-                });
+            if (CultureService.GetCultureForLanguage().Equals(RTLLanguage)) {
+                Device.BeginInvokeOnMainThread(() => { popup.FlowDirection = FlowDirection.RightToLeft; });
             }
-            else
-            {
-                Device.BeginInvokeOnMainThread(() => 
-                {
-                  //  this.FlowDirection = FlowDirection.LeftToRight;
-                    popup.FlowDirection = FlowDirection.LeftToRight;
-                });
+            else {
+                Device.BeginInvokeOnMainThread(() => { popup.FlowDirection = FlowDirection.LeftToRight; });
             }
         }
-        //private async void Button_Clicked(object sender, EventArgs e)
-        //{
-        //    await Navigation.PushAsync(new SecondPage(this));
-        //}
 
-        //void OnTapGestureRecognizerTapped(object sender, EventArgs args)
-        //{
-        //    this.popup.Show();
-        //}
-        //private void Button_Clicked_1(object sender, EventArgs e)
-        //{
-        //    this.popup.Show();
-        //}
-
-        //private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
-        //{
-        //    this.popup.Show();
-        //}
     }
 }
--- PrismPopup/PrismPopup/PrismPopup/Views/MenuPage.xaml.cs
+++ PrismPopup/PrismPopup/PrismPopup/Views/MenuPage.xaml.cs
@@ -20,33 +20,15 @@ namespace PrismPopup.Views
             this.Main = mainPage;
         }
 
-        //private async void Button_Clicked(object sender, System.EventArgs e)
-        //{
-            
-        //}
-
-        //private void Button_Clicked_1(object sender, System.EventArgs e)
-        //{
-
-        //}
-
-        //private void Button_Clicked_2(object sender, System.EventArgs e)
-        //{
-
-        //}
-
         private void LTRButton_Clicked(object sender, System.EventArgs e)
         {
             CultureService.SetCultureForLanguage(LTRLanguage);
-            //DependencyService.Get<ILocalize>().SetLocale(new CultureInfo("en-US"));
-            //Main.SetRTL("en-US");
+
         }
 
         private void RTLButton_Clicked(object sender, System.EventArgs e)
         {
             CultureService.SetCultureForLanguage(RTLLanguage);
-            //DependencyService.Get<ILocalize>().SetLocale(new CultureInfo("ar"));
-            //Main.SetRTL("ar");
         }
 
         private async void NavigateToMainPageButton_Clicked(object sender, System.EventArgs e)
